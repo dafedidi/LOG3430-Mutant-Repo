9f58ada8416df31ef3d70aa21454d29b8a84497d
Mo-LK <mounir.lammali@polymtl.ca>
2024-02-16 09:56:04 -0500

    Add test to deal with mutant

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -1,13 +1,18 @@
 import unittest
+from unittest.mock import patch
 from CCoinBox import CCoinBox
 
 class Test_CCoinBox(unittest.TestCase):
-
-    def test_pass(self):
-        pass
 
     def test_monnaie(self):
         coinBox = CCoinBox()
         coinBox.ajouter_25c()
         coinBox.ajouter_25c()
         self.assertEqual(coinBox.get_vente_permise(), True)
+
+    def test_vente(self):
+        coinBox = CCoinBox()
+        coinBox.ajouter_25c()
+        coinBox.ajouter_25c()
+        coinBox.vente()
+        self.assertEqual(coinBox.get_monnaie_totale(), 0)

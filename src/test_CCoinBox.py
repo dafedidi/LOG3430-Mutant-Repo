458cd0d30e0317266987cb9ac47ac1f501f72dd6
Ralph El Khoury <ralph.el-khoury@polymtl.ca>
2024-02-16 10:07:28 -0500

    testMutants

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -11,3 +11,15 @@ class Test_CCoinBox(unittest.TestCase):
         coinBox.ajouter_25c()
         coinBox.ajouter_25c()
         self.assertEqual(coinBox.get_vente_permise(), True)
+
+    def test_retourne_monnaie(self):
+        coinBox = CCoinBox()
+        coinBox.retourne_monnaie()
+        self.assertEqual(coinBox.vente_permise, False)
+
+    def test_vente(self):
+        coinBox = CCoinBox()
+        coinBox.ajouter_25c()
+        coinBox.ajouter_25c()
+        coinBox.vente()
+        self.assertEqual(coinBox.vente_permise, True)

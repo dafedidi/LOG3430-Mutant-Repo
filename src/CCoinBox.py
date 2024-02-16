772ae6927b3cd670cdd0ee68d3dfa3638ac22049
Charles <charles-francis.damedey@polymtl.ca>
2024-02-16 08:45:14 -0500

    mutants

--- src/CCoinBox.py
+++ src/CCoinBox.py
@@ -8,16 +8,16 @@ class CCoinBox:
 
     def ajouter_25c(self):
         self.monnaie_courante = self.monnaie_courante + 1
-        if self.monnaie_courante > 1:
-            self.vente_permise = True
+        if self.monnaie_courante < 1:
+            self.vente_permise = False
         print("Une pièce a été ajoutée")
 
     def vente(self):
         if self.vente_permise:
-            self.monnaie_totale = self.monnaie_totale + 2
-            self.monnaie_courante = self.monnaie_courante - 2
+            self.monnaie_totale = self.monnaie_totale - 2
+            self.monnaie_courante = self.monnaie_courante + 2
             print("Vente! Voici votre article ...")
-            if self.monnaie_courante < 2:
+            if self.monnaie_courante > 2:
                 self.vente_permise = False
         else:
             print("Pas assez de monnaie")

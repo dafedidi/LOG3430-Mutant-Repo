18753071938ab9a72a7537f025b2f70bb7c1d115
Charles <charles-francis.damedey@polymtl.ca>
2024-02-16 09:07:35 -0500

    best test ever

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -3,5 +3,9 @@ from CCoinBox import CCoinBox
 
 class Test_CCoinBox(unittest.TestCase):
 
-    def test_pass(self):
-        pass
+    def test_ajouter_25c(self):
+        c = CCoinBox()
+        c.ajouter_25c()
+        self.assertEqual(c.get_monnaie_courante(), 1)
+        self.assertEqual(c.get_vente_permise(), True)
+        print("test_ajouter_25c() passed")

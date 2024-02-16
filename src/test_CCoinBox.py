30634bb11e5420bdb936234e5de6d47a413003ce
GharibSidney <sidney-gharib@hotmail.com>
2024-02-16 09:56:02 -0500

    commit

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -11,3 +11,8 @@ class Test_CCoinBox(unittest.TestCase):
         coinBox.ajouter_25c()
         coinBox.ajouter_25c()
         self.assertEqual(coinBox.get_vente_permise(), True)
+
+    def test_monnaie_Negative(self):
+        coinBox = CCoinBox()
+        coinBox.ajouter_25c()
+        self.assertEqual(self.monnaie_courante, 1)
